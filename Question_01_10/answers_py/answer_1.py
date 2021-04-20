52c14cea315ddf065d232878e30c60e840c89c7c
Jack <lusi2114@gmail.com>
2021-04-20 09:53:10 +0800

    update

--- Question_01_10/answers_py/answer_1.py
+++ Question_01_10/answers_py/answer_1.py
@@ -1,7 +1,15 @@
-import cv2
+"""
+OpenCV is BGR, Pillow is RGB
 
+When reading a color image file, OpenCV imread() reads as a NumPy array ndarray of row (height) x column (width) x color (3).
+The order of color is BGR (blue, green, red).
+"""
+
+import cv2
+import numpy as np
 
 # function: BGR -> RGB
+from PIL import Image
 
 
 def BGR2RGB(img):
@@ -20,10 +28,12 @@ def BGR2RGB(img):
 # Read image
 img = cv2.imread("../imori.jpg")
 
+im_pillow = np.array(Image.open('data/src/lena.jpg'))
+
 # BGR -> RGB
 img_rgb = BGR2RGB(img)
 print('use BGR2RGB')
-cv2.imshow("result", img_rgb)
+cv2.imshow("BGR2RGB", img_rgb)
 cv2.waitKey(0)
 
 print('use opencv')
